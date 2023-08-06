01ae2866a16de473eecccd797aa0473216cf3607
Anderson Silva <andssilva.23@gmail.com>
2023-08-06 18:41:05 -0400

    [A] feat: :rocket: criando componente para card de post principal

---
+++ one_page_angular/src/app/shared/components/featured-slider/featured-slider.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-slider',
+  templateUrl: './featured-slider.component.html',
+  styleUrls: ['./featured-slider.component.sass']
+})
+export class FeaturedSliderComponent {
+
+}
